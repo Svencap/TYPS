16f8454cca0d13190083a4f2edb728b50800f81c
Svencap <Ayzekmanduar@mail.ru>
2022-12-22 01:29:18 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Param {
+  id: number;
+  name: string;
+  type: string;
 }
 
-export default App;
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+interface State {
+  params: Param[];
+  model: Model;
+}
+
+class ParamEditor extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { params: props.params, model: props.model };
+  }
+
+  changeModel = (id: number | undefined, text: string) => {
+    const model = this.getModel();
+    const newModel = model.paramValues.reduce((acc, data): any => {
+      const newValue = data.paramId === id ? text : data.value;
+      const newParam = { paramId: data.paramId, value: newValue };
+      const newParams = [...acc.paramValues, newParam];
+      return { paramValues: newParams };
+    }, { paramValues: [] });
+    return this.setState({ params: this.state.params, model: newModel });
+  }
+
+  getModel(): Model {
+    return this.state.model;
+  }
+
+  render() {
+    const { params, model } = this.state;
+    const { paramValues } = model;
+    const style = { 'display': 'flex', 'alignItems': 'center', 'gap': '10px' };
+    return (
+      <div>
+        {params.map(({ id, name }) => {
+        const getParamValue = paramValues.find(({ paramId }) => paramId === id);
+        return (
+        <div key={id} style={style}>
+          <p key={id}>{name}</p>
+          <input type="text" onBlur={({ target }) => this.changeModel(getParamValue?.paramId, target.value)} defaultValue={getParamValue?.value}/>
+        </div>)
+        })}
+      </div>
+    );
+  }
+}
+
+export default ParamEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ParamEditer from "./App";
+
+const params = [
+  {
+    id: 1,
+    name: "Назначение",
+    type: "string",
+  },
+  {
+    id: 2,
+    name: "Длина",
+    type: "string",
+  },
+];
+
+const model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: "повседневное",
+    },
+    {
+      paramId: 2,
+      value: "макси",
+    },
+  ],
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ParamEditer params={params} model={model} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
