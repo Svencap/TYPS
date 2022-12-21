b6e7d572068a4b1090b8ece84d7314b5ea3b6b81
Svencap <Ayzekmanduar@mail.ru>
2022-12-22 06:11:51 +0900

    add version_1

--- src/App.tsx
+++ src/App.tsx
@@ -25,13 +25,37 @@ interface State {
   model: Model;
 }
 
+
+interface PropsTextField {
+  id: number | undefined,
+  type: any,
+  changeModel: any,
+  value: any
+}
+
+ 
+class TextField extends React.Component<PropsTextField> {
+  constructor(props: PropsTextField) {
+    super(props);
+    this.state = { type: props.type, value: props.value, changeModel: props.changeModel};
+  }
+  render() {
+    const { id, type, value, changeModel } = this.props;
+    return (
+      <input type={type} onChange={() => changeModel(id, value)} defaultValue={value}/>
+    )
+  }
+}
+
+
+
 class ParamEditor extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = { params: props.params, model: props.model };
   }
 
-  changeModel = (id: number | undefined, text: string) => {
+  changeModel = (id: number | undefined, text: any) => {
     const model = this.getModel();
     const newModel = model.paramValues.reduce((acc, data): any => {
       const newValue = data.paramId === id ? text : data.value;
@@ -39,7 +63,7 @@ class ParamEditor extends React.Component<Props, State> {
       const newParams = [...acc.paramValues, newParam];
       return { paramValues: newParams };
     }, { paramValues: [] });
-    return this.setState({ params: this.state.params, model: newModel });
+    this.setState({ params: this.state.params, model: newModel });
   }
 
   getModel(): Model {
@@ -52,12 +76,12 @@ class ParamEditor extends React.Component<Props, State> {
     const style = { 'display': 'flex', 'alignItems': 'center', 'gap': '10px' };
     return (
       <div>
-        {params.map(({ id, name }) => {
+        {params.map(({ id, name, type }) => {
         const getParamValue = paramValues.find(({ paramId }) => paramId === id);
         return (
         <div key={id} style={style}>
           <p key={id}>{name}</p>
-          <input type="text" onBlur={({ target }) => this.changeModel(getParamValue?.paramId, target.value)} defaultValue={getParamValue?.value}/>
+          <TextField id={getParamValue?.paramId} type={type} value={getParamValue?.value} changeModel={this.changeModel}/>
         </div>)
         })}
       </div>
